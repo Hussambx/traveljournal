5d8151dd0c53bcaae6e3e07eddf191829bbb476c
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-07-28 17:03:48 -0400

    Added Image to Nav, and some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 
 
-
 export default function Header(){
     return(
   <nav>
-    <h1>Travek Journal</h1>
+    <img src="https://www.freepnglogos.com/uploads/globe-png/file-blank-globe-svg-wikimedia-commons-40.png"/>
+    <h1>Travel Journal</h1>
   </nav>
 
 
