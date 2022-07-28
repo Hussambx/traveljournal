8542d0d6696c92986f32521f61516dbbcff3db6e
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-07-28 17:53:00 -0400

    Setup Props For Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,19 @@
 import React from "react"
 
-export default function Card(){
+export default function Card(props){
     return(
-        <div>
+        <div className="main">
+            <div className="left">
+            <img src={props.img}/>
+            </div>
+            <div className="right">
+            <span>Hi</span>
+            <span>Bye</span>
+            <h2>{props.title}</h2>
+            <span>{props.startDate} - {props.endDate}</span>
+            <p>{props.description}</p>
+            
+            </div>
             
         </div>
     )
