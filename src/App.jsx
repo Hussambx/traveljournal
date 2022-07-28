ed3d209e51c7b81d81b58a72d62faba72e5c853f
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-07-28 17:17:41 -0400

    Added Data.jsx and imported into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/Header'
-
+import Cards from "./components/Cards"
+import data from "./assets/data"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
   <Header/>
+  <Cards/>
   </div>
   )
 }
