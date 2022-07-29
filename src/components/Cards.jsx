95d1b1ae6373234abfcd9f8aaa8b993fbcffba89
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-07-29 14:47:21 -0400

    More Formating Improvements

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,7 @@ export default function Card(props){
             </div>
             <div className="right">
             <span>{props.location}         </span>
-            <span><a href ="www.google.ca">View On Google Maps</a></span>
+            <span><a href ="www.google.ca">View Location</a></span>
             <h2>{props.title}</h2>
             <span>{props.startDate} - {props.endDate}</span>
             <p>{props.description}</p>
